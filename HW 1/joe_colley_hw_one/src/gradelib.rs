a512f6681dca4c2206aa578f99cbf0fcc81e2994
Joe Colley <josephthco@gmail.com>
2023-08-25 11:11:03 -0400

    changs

--- HW 1/joe_colley_hw_one/src/gradelib.rs
+++ HW 1/joe_colley_hw_one/src/gradelib.rs
@@ -1,8 +1,8 @@
-pub mod gradelib{
+pub mod gradelib {
     use std::fs::File;
     use std::io::prelude::*;
     use std::path::Path;
-    
+
     pub fn grade_str(g: f64) -> String {
         format!("{{\"score\": {},\"output\":\"Grader ran successfully\",\"stdout_visibility\":\"visible\"}}", g)
     }
@@ -13,7 +13,7 @@ pub mod gradelib{
             Err(_) => panic!("file"),
             Ok(f) => f,
         };
-        match file.write_all(grade_str(g).as_bytes())  {
+        match file.write_all(grade_str(g).as_bytes()) {
             Err(_why) => panic!("couldn't write file"),
             Ok(_) => println!("wrote json file"),
         }
